d1632854fbc06d8e8b8f41dbd76ba2f96f0d209a
ravilella1234 <masters.selenium@gmail.com>
2024-05-01 07:59:22 +0530

    Java StringS

---
+++ ThirdProject/hdfc/loans/homeloans/TwoDimenssionArray.java
@@ -0,0 +1,34 @@
+package hdfc.loans.homeloans;
+
+public class TwoDimenssionArray 
+{
+
+	public static void main(String[] args) 
+	{
+		int[][] a = {
+						{10,20,30},
+						{40,50},
+						{70,80,90,10,20}
+					};
+		
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<a[i].length;j++)
+			{
+				System.out.print(a[i][j]+"  ");
+			}
+			System.out.println();
+		}
+		
+		//For Eachloop
+		for(int[] i:a)
+		{
+			for(int j:i)
+			{
+				System.out.print(j+"  ");
+			}
+			System.out.println();
+		}
+		
+	}
+}
