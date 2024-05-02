b0fd9478f06155ea27f78660f3d28de6804341b2
ravilella1234 <masters.selenium@gmail.com>
2024-05-02 08:05:55 +0530

    Exception Day-1

---
+++ ThirdProject/exceptionHandling/ExceptionDemo.java
@@ -0,0 +1,34 @@
+package exceptionHandling;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class ExceptionDemo 
+{
+
+	public static void main(String[] args) 
+	{
+		int a,b,c;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) 
+		{
+			System.out.println("Enter the A value :");
+			a = sc.nextInt();
+			System.out.println("Enter the B value :");
+			b = sc.nextInt();
+			
+			try 
+			{
+				c = a / b;
+				System.out.println(c);
+				break;
+			} 
+			catch (Exception e) 
+			{
+				e.printStackTrace();
+			} 			
+		}
+	}
+}
