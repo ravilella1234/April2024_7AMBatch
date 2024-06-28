b54b2ec8c6e11dca503d6420c16665c4164fdb59
ravilella1234 <masters.selenium@gmail.com>
2024-06-28 07:48:54 +0530

    TestNG Day-5

---
+++ May2024_7AM_SeleniumBatch/src/test/java/testNGpack/OneTestValueToAnotherTest.java
@@ -0,0 +1,24 @@
+package testNGpack;
+
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+public class OneTestValueToAnotherTest 
+{
+	
+  @Test
+  public void f1(ITestContext context) 
+  {
+	  String id = "RRR999";
+	  System.out.println("From f1 : " + id);
+	  context.setAttribute("idValue", id);
+  }
+  
+  @Test
+  public void f2(ITestContext context) 
+  {
+	  String id = (String)context.getAttribute("idValue");
+	  System.out.println("From f1 : " + id);
+  }
+  
+}
